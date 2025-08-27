ae7abaf6f965158d9a6898d28cad34cd9ea0595c
Sand82 <sandoki@abv.bg>
2025-08-27 17:37:18 +0300

    added Carter, MapsterMapper, MediatorR libraries, building blocks service and crate product endpoint

--- eshop-microservices/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,6 +1,26 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct;
+
+ public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+     : ICommand<CreateProductResult>;
+ public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler 
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public class CreateProductHandler 
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
+        var roduct = new Product
+        { 
+            Name = command.Name,
+            Category = command.Category,
+            Descrption = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price,
+        };
+
+        return new CreateProductResult(Guid.NewGuid());
     }
 }
--- eshop-microservices/src/Services/Catalog/Catalog.API/Program.cs
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Program.cs
@@ -1,10 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config => 
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
 //Configure 5the HTTP request pipeline.
+app.MapCarter();
 
 app.Run();
  
