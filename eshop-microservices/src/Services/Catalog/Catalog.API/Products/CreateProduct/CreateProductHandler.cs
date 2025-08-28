124a88865d496b6ccd32d97e8514145c748716c2
Sand82 <sandoki@abv.bg>
2025-08-28 17:05:01 +0300

    added docker compose and configurations for Carter library

--- eshop-microservices/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,18 +1,15 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
+﻿namespace Catalog.API.Products.CreateProduct;
 
-namespace Catalog.API.Products.CreateProduct;
-
- public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
      : ICommand<CreateProductResult>;
- public record CreateProductResult(Guid Id);
+public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler 
+internal class CreateProductCommandHandler(IDocumentSession session)
     : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        var roduct = new Product
+        var product = new Product
         { 
             Name = command.Name,
             Category = command.Category,
@@ -21,6 +18,9 @@ internal class CreateProductCommandHandler
             Price = command.Price,
         };
 
-        return new CreateProductResult(Guid.NewGuid());
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new CreateProductResult(product.Id);
     }
 }
