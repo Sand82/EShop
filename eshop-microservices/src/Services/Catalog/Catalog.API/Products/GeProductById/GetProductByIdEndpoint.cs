e7268caeeb9cc2f3e1384f29a60559867e12c33e
Sand82 <sandoki@abv.bg>
2025-08-29 18:26:35 +0300

    added new end points to the Catalog microservice

---
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Products/GeProductById/GetProductByIdEndpoint.cs
@@ -0,0 +1,26 @@
+﻿
+using Catalog.API.Products.GetProducts;
+
+namespace Catalog.API.Products.GeProductById
+{
+    public record GetProductByIdResponce(Product Product);
+    public class GetProductByIdEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductByIdQuery(id));
+
+                var responce = result.Adapt<GetProductByIdResponce>();
+
+                return Results.Ok(responce);
+            })
+            .WithName("GetProductById")
+            .Produces<GetProductsResponce>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Product By Id")
+            .WithDescription("Get Product By Id");
+        }
+    }
+}
---
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,24 @@
+﻿namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductsResponce(IEnumerable<Product> Products);
+
+    public class GetProductsEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async (ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductsQuery());
+
+                var responce = result.Adapt<GetProductsResponce>();
+
+                return Results.Ok(responce);
+            })
+            .WithName("GetProducts")
+            .Produces<GetProductsResponce>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products")
+            .WithDescription("Get Products"); 
+        }
+    }
+}
