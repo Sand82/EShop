1a7b7c9f7bd8a53704e1b7e950a4a2fd7a779704
Sand82 <sandoki@abv.bg>
2025-09-26 10:02:04 +0300

    Added store basket functionality

--- eshop-microservices/src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ eshop-microservices/src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,5 +1,25 @@
 ﻿namespace Basket.API.Basket.StoreBasket;
 
-public class StoreBasketHandler
+public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+public record StoreBasketResult(string UserName);
+
+public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
 {
+    public StoreBasketCommandValidator()
+    {
+        RuleFor(x => x.Cart).NotNull().WithMessage("Cart can not be null.");
+        RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("User name is required.");
+    }
+}
+
+internal class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+    {
+        ShoppingCart cart = command.Cart;
+
+        //TODO:
+
+        return new StoreBasketResult("Sand");
+    }
 }
