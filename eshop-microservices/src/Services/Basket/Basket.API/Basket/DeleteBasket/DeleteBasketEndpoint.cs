6cc1de7ec053c22779b7388cb6b9d489475794fe
Sand82 <sandoki@abv.bg>
2025-09-30 08:26:45 +0300

    added delete basket functionality

--- eshop-microservices/src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
+++ eshop-microservices/src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
@@ -1,5 +1,26 @@
-﻿namespace Basket.API.Basket.DeleteBasket;
+﻿using Basket.API.Basket.StoreBasket;
 
-public class DeleteBasketEndpoint
+namespace Basket.API.Basket.DeleteBasket;
+
+public record DeleteBasketRequest(string UserName);
+public record DelateBasketResponse(bool IsSuccess);
+
+public class DeleteBasketEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/basket/{userName}", async (string userName, ISender sender ) =>
+        {
+            var result = await sender.Send( new DeleteBasketRequest(userName));
+
+            var response = result.Adapt<DelateBasketResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("DeleteBasket")
+        .Produces<StoreBasketResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Delete Basket")
+        .WithDescription("Delete Basket");
+    }
 }
--- eshop-microservices/src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ eshop-microservices/src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -1,5 +1,22 @@
 ﻿namespace Basket.API.Basket.DeleteBasket;
 
-public class DeleteBasketHandler
+public record DelateBasketCommand(string UserName) : ICommand<DelateBasketResult>;
+public record DelateBasketResult(bool IsSuccess);
+
+public class DelateBasketCommandValidator : AbstractValidator<DelateBasketCommand>
 {
+    public DelateBasketCommandValidator()
+    {
+        RuleFor(x => x.UserName).NotEmpty().WithMessage("User name is required.");
+    }
+}
+
+public class DeleteBasketCommandHandler : ICommandHandler<DelateBasketCommand, DelateBasketResult>
+{
+    public async Task<DelateBasketResult> Handle(DelateBasketCommand command, CancellationToken cancellationToken)
+    {
+        //TODO
+
+        return new DelateBasketResult(true);
+    }
 }
