addb4f5348a1e9e0651400aeaf15f7500c440a45
Sand82 <sandoki@abv.bg>
2025-10-29 09:49:11 +0200

    added grpc method get discount in discount service

--- eshop-microservices/src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
+++ eshop-microservices/src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -1,15 +1,23 @@
 ﻿using Discount.Grpc.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace Discount.Grpc.Data
-{
-    public class DiscountContext : DbContext
-    {
-        public DbSet<Coupon> Coupons { get; set; }
+namespace Discount.Grpc.Data;
 
-        public DiscountContext(DbContextOptions<DiscountContext> options) 
-            : base(options)
-        {           
-        }
+public class DiscountContext : DbContext
+{
+    public DbSet<Coupon> Coupons { get; set; }
+
+    public DiscountContext(DbContextOptions<DiscountContext> options) 
+        : base(options)
+    {           
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Coupon>().HasData(
+                new Coupon { Id = 1, ProductName = "IPhone X", Description = "IPhone X, grate way to be connected!", Amount = 150},
+                new Coupon { Id = 2, ProductName = "Samsung 10", Description = "Best of the best!", Amount = 150}
+            );
+
     }
 }
