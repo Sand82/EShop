dec24cc8a3fefbc5e31fd85d555e2c776a9325bf
Sand82 <sandoki@abv.bg>
2025-11-05 10:18:52 +0200

    added abstractions under ordering service domain layer

---
+++ eshop-microservices/src/Services/Ordering/Ordering.Domain/Abstractions/Aggregate.cs
@@ -0,0 +1,21 @@
+﻿namespace Ordering.Domain.Abstractions;
+
+public abstract class Aggregate<TId> : Entity<TId>, IAggregate<TId>
+{
+    private readonly List<IDomainEvent> _domainEvents = new(); 
+    public IReadOnlyList<IDomainEvent> DomainEvents => throw new NotImplementedException();
+
+    public void AddIDomainEvents(IDomainEvent domainEvents)
+    {
+        _domainEvents.Add(domainEvents);
+    }
+
+    public IDomainEvent[] ClearDomainEvents()
+    {
+        IDomainEvent[] dequeuedEvents = _domainEvents.ToArray();
+
+        _domainEvents.Clear();
+
+        return dequeuedEvents;
+    }
+}
