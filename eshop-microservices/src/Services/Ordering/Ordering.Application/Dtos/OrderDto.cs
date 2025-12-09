1fcdb2cae52d1c0dabc893d291f05005be92ea7c
Sand82 <sandoki@abv.bg>
2025-12-09 15:30:48 +0200

    added application db context class and create order command and command handler

---
+++ eshop-microservices/src/Services/Ordering/Ordering.Application/Dtos/OrderDto.cs
@@ -0,0 +1,13 @@
+﻿using Ordering.Domain.Enums;
+
+namespace Ordering.Application.Dtos;
+
+public record OrdeDto(
+    Guid Id,
+    Guid CustomerId,
+    string OrderName,
+    AddressDto ShippingAddress,
+    AddressDto BillingAddress,
+    PaymentDto Payment,
+    OrderStatus Status,
+    List<OrderItemsDto> OrderItems);
