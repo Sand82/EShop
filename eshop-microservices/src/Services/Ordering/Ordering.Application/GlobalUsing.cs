dca787be038d6ed08cc8ba36edda4bd89bbca12c
Sand82 <sandoki@abv.bg>
2025-12-10 10:47:44 +0200

    added notification handlers for order update and create operations

--- eshop-microservices/src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ eshop-microservices/src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -4,3 +4,7 @@ global using Ordering.Application.Dtos;
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
 global using BuildingBlocks.Exceptions;
+global using FluentValidation;
+global using MediatR;
+global using Microsoft.Extensions.Logging;
+global using Ordering.Domain.Events;
---
+++ eshop-microservices/src/Services/Ordering/Ordering.Application/Orders/EventHandlers/OrderUpdatedEventHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace Ordering.Application.Orders.EventHandlers;
+
+public class OrderUpdatedEventHandler(ILogger<OrderUpdatedEventHandler> logger)
+    : INotificationHandler<OrderCreatedEvent>
+{
+    public Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Domain Event handler: {DomainEvent}", notification.GetType().Name);
+        
+        return Task.CompletedTask;
+    }
+}
