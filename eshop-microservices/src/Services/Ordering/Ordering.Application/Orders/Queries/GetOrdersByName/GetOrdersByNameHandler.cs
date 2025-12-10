28c79399412e7156f50d330890eaf94a81308545
Sand82 <sandoki@abv.bg>
2025-12-10 15:33:10 +0200

    added get orders by customer query functionality

--- eshop-microservices/src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameHandler.cs
+++ eshop-microservices/src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameHandler.cs
@@ -11,52 +11,8 @@ public class GetOrdersByNameHandler(IApplicationDbContext dbContext) : IQueryHan
             .AsNoTracking()
             .Where(o => o.OrderName.Value!.Contains(query.Name))
             .OrderBy(o => o.OrderName)
-            .ToListAsync(cancellationToken);
+            .ToListAsync(cancellationToken);        
 
-        var orderDtos = ProjectToOrderDto(orders);
-
-        return new GetOrdersByNameResult(orderDtos);
-    }
-
-    private List<OrderDto> ProjectToOrderDto(List<Order> orders)
-    {
-        List<OrderDto> result = new();
-
-        foreach (var order in orders) 
-        {
-            var orderDto = new OrderDto(
-                Id: order.Id.Value,
-                CustomerId: order.CustomerId.Value,
-                OrderName: order.OrderName.Value,
-                ShippingAddress: new AddressDto(
-                    order.ShippingAddress.FirsName,
-                    order.ShippingAddress.LastName,
-                    order.ShippingAddress.EmailAddress!,
-                    order.ShippingAddress.AddressLine,
-                    order.ShippingAddress.Country,
-                    order.ShippingAddress.State,
-                    order.ShippingAddress.ZipCode),
-                BillingAddress: new AddressDto(
-                    order.BillingAddress.FirsName,
-                    order.BillingAddress.LastName,
-                    order.BillingAddress.EmailAddress!,
-                    order.BillingAddress.AddressLine,
-                    order.BillingAddress.Country,
-                    order.BillingAddress.State,
-                    order.BillingAddress.ZipCode),
-                Payment: new PaymentDto(
-                    order.Payment.CardName!,
-                    order.Payment.CardNumber,
-                    order.Payment.Expiration,
-                    order.Payment.CVV,
-                    order.Payment.PaymentMethod),
-                Status: order.Status,
-                OrderItems: order.OrderItems.Select(oi => new OrderItemsDto(oi.OrderId.Value, oi.ProductId.Value, oi.Quantity, oi.Price)).ToList()
-                );
-
-            result.Add(orderDto);
-        }
-
-        return result;
-    }
+        return new GetOrdersByNameResult(orders.ToOrderDtoList());
+    }    
 }
