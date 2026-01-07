325ea505626b6e0b68202d85be8e85c776ab345d
Sand82 <sandoki@abv.bg>
2026-01-07 16:26:00 +0200

    added IBasket and IOrdering services consumed by Refit HttpClientFactory

---
+++ eshop-microservices/src/WebApps/Shopping.Web/Services/IOrderingService.cs
@@ -0,0 +1,13 @@
+﻿namespace Shopping.Web.Services;
+
+public interface IOrderingService
+{
+    [Get("/ordering-service/orders?pageIndex={pageIndex}&pageSize={pageSize}")]
+    Task<GetOrderResponse> GetOrders(int? pageIndex = 1, int? pageSize = 10);
+
+    [Get("/ordering-service/orders/{orderName}")]
+    Task<GetOrderByNameResponse> GetOrdersByName(string orderName);
+
+    [Get("/ordering-service/orders/customer/{customerId}")]
+    Task<GetOrderByCustomerResponse> GetOrdersByCustomer(Guid customerId);
+}
